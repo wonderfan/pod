1931fba02537494f43d16e36896801949af5b666
jason <fanjiahe2000@163.com>
2024-06-05 07:08:02 +0000

    chore: rust

--- main.sh
+++ main.sh
@@ -1,2 +1,7 @@
 git clone --depth 1 https://github.com/bluealloy/revm.git
 git clone --depth 1 https://github.com/a16z/helios.git
+git clone --depth 1 https://github.com/rust-ethereum/evm.git
+git clone --depth 1 https://github.com/succinctlabs/sp1-reth.git
+git clone --depth 1 https://github.com/paradigmxyz/reth.git
+git clone --depth 1 https://github.com/risc0/zeth.git
+git clone --depth 1 https://github.com/risc0/risc0-ethereum.git
