befd76aea158562e85d72c13904ee00807ce018e
jason <fanjiahe2000@163.com>
2024-06-09 11:26:10 +0000

    chore: bash script

--- zkp.sh
+++ zkp.sh
@@ -1,1 +1,4 @@
 # zero-knowledge repository
+git clone --depth 1 https://github.com/starknet-io/starkgate-contracts.git
+git clone --depth 1 https://github.com/starkware-libs/cairo-lang.git
+git clone --depth 1 https://github.com/starkware-libs/cairo.git
