2952b2373b7b975d839a226d4a74be7f76ee555f
jason <fanjiahe2000@163.com>
2024-06-30 11:08:29 +0000

    chore: module blockchain

---
+++ module.sh
@@ -0,0 +1,3 @@
+git clone --depth 1 https://github.com/celestiaorg/celestia-node
+git clone --deptht 1 https://github.com/celestiaorg/celestia-core
+git clone --depth 1 https://github.com/celestiaorg/celestia-app
