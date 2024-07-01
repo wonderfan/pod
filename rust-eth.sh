4d2e10f898922dcf234d244b709997316c8882f0
jason <fanjiahe2000@163.com>
2024-07-01 16:21:05 +0000

    chore: realtime ethereum

--- rust-eth.sh
+++ rust-eth.sh
@@ -5,3 +5,4 @@ git clone --depth 1 https://github.com/succinctlabs/sp1-reth.git
 git clone --depth 1 https://github.com/paradigmxyz/reth.git
 git clone --depth 1 https://github.com/risc0/zeth.git
 git clone --depth 1 https://github.com/risc0/risc0-ethereum.git
+git clone --depth 1 https://github.com/megaeth-labs/evmone-compiler.git
