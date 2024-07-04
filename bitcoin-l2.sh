3d050dca65ae5325097e70c331981af8493d5d06
jason <fanjiahe2000@163.com>
2024-07-04 02:20:05 +0000

    chore: lighting network

--- bitcoin-l2.sh
+++ bitcoin-l2.sh
@@ -1,3 +1,4 @@
 git clone --depth 1 https://github.com/stacks-network/stacks-core.git
 git clone --depth 1 https://github.com/cloudhead/nakamoto.git
 git clone --depth 1 https://github.com/RGB-WG/rgb-core.git
+git clone --depth 1 https://github.com/lightningdevkit
