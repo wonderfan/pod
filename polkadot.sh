ca7bd9eb195447893b75f3c455a5e1eae516a948
jason <fanjiahe2000@163.com>
2024-07-22 01:13:16 +0000

    chore: polkadot network

--- polkadot.sh
+++ polkadot.sh
@@ -1,1 +1,3 @@
 git clone --depth 1 https://github.com/paritytech/polkadot-sdk.git
+git clone --depth 1 https://github.com/smol-dot/smoldot.git
+git clone --depth 1 https://github.com/paritytech/subxt.git
