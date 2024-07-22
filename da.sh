6ea32d961f1673fd879e1f47d8a0812fe0d2757e
jason <fanjiahe2000@163.com>
2024-07-22 01:18:55 +0000

    chore: polkadot network

---
+++ da.sh
@@ -0,0 +1,3 @@
+git clone --depth 1 https://github.com/availproject/avail.git
+git clone --depth 1 https://github.com/rollkit/rollkit.git
+git clone --depth 1 https://github.com/celestiaorg/celestia-node.git
