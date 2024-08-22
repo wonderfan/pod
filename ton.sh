23bf632332758240de070a10be5b878727406b04
jason <fanjiahe2000@163.com>
2024-08-22 02:33:43 +0000

    chore: ton network

--- ton.sh
+++ ton.sh
@@ -4,3 +4,4 @@ git clone --depth 1 https://github.com/ston-fi/tonlib-rs.git
 git clone --depth 1 https://github.com/VanishMax/foton.git
 git clone --depth 1 https://github.com/ton-org/ton.git
 git clone --depth 1 https://github.com/toncenter/tonweb.git
+git clone --depth 1 https://github.com/ton-org/ton-core.git
