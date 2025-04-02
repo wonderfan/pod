467e44f6a599d284b845fbb9aa5641c7208ba14b
wonderfan <fanjiahe2000@163.com>
2025-04-02 08:14:30 +0000

    chore: daily update

--- ai.sh
+++ ai.sh
@@ -1,3 +1,4 @@
 git clone --depth 1 https://github.com/FlowiseAI/Flowise
 git clone --depth 1 https://github.com/trypromptly/LLMStack
 git clone --depth 1 https://github.com/crewAIInc/crewAI
+git clone --depth 1 https://github.com/n8n-io/n8n
