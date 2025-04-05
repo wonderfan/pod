736913d1835f382dc6274ab82357186645e3971d
wonderfan <fanjiahe2000@163.com>
2025-04-05 01:02:06 +0000

    chore: daily update

--- video.sh
+++ video.sh
@@ -1,2 +1,5 @@
 git clone --depth 1 https://github.com/Zulko/moviepy
 git clone --depth 1 https://github.com/Breakthrough/PySceneDetect
+git clone --depth 1 https://github.com/WyattBlue/auto-editor
+git clone --depth 1 https://github.com/abhiTronix/vidgear
+git clone --depth 1 https://github.com/OpenTalker/video-retalking
