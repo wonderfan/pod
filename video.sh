0487db9a12e5bb2271441810c79c59cb082cbb10
wonderfan <fanjiahe2000@163.com>
2025-04-05 14:21:34 +0000

    chore: daily update

--- video.sh
+++ video.sh
@@ -4,3 +4,4 @@ git clone --depth 1 https://github.com/WyattBlue/auto-editor
 git clone --depth 1 https://github.com/abhiTronix/vidgear
 git clone --depth 1 https://github.com/OpenTalker/video-retalking
 git clone --depth 1 https://github.com/python-pillow/Pillow
+git clone --depth 1 https://github.com/basswood-io/BasswoodAV
