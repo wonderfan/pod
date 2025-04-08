17919d0b2242c6118d23d864e51bfac064658265
wonderfan <fanjiahe2000@163.com>
2025-04-08 23:26:02 +0000

    chore: make updates

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
 alias code='vim -n -c "set rnu"'
+alias push='git add --all && git commit -m "chore: make updates" && git push'
--- ai.sh
+++ ai.sh
@@ -4,3 +4,5 @@ git clone --depth 1 https://github.com/crewAIInc/crewAI
 git clone --depth 1 https://github.com/n8n-io/n8n
 git clone --depth 1 https://github.com/meta-llama/llama-models
 git clone --depth 1 https://github.com/huggingface/smollm.git
+git clone --depth 1 https://github.com/huggingface/pytorch-image-models
+git clone --depth 1 https://github.com/openai/tiktoken
