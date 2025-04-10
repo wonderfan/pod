3e7e6c402f4be52140d7bb5fc53cee1b9a0d46e5
wonderfan <fanjiahe2000@163.com>
2025-04-10 23:21:06 +0000

    chore: make updates

--- module.sh
+++
@@ -1,3 +0,0 @@
-git clone --depth 1 https://github.com/celestiaorg/celestia-node
-git clone --deptht 1 https://github.com/celestiaorg/celestia-core
-git clone --depth 1 https://github.com/celestiaorg/celestia-app
