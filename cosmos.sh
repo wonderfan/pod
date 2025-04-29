ce20950402faad29dbbf5992506527bf0457cacf
wonderfan <fanjiahe2000@163.com>
2025-04-29 01:25:55 +0000

    chore: make updates

--- cosmos.sh
+++ cosmos.sh
@@ -1,6 +1,7 @@
-git clone --depth 1 https://github.com/cosmos/ibc-go.git
-git clone --depth 1 https://github.com/cosmos/cosmos-sdk.git
-git clone --depth 1 https://github.com/celestiaorg/celestia-node
-git clone --deptht 1 https://github.com/celestiaorg/celestia-core
-git clone --depth 1 https://github.com/celestiaorg/celestia-app
-git clone --depth 1 https://github.com/osmosis-labs/osmosis.git
+git clone --depth 1 https://github.com/sei-protocol/sei-chain.git
+# git clone --depth 1 https://github.com/cosmos/ibc-go.git
+# git clone --depth 1 https://github.com/cosmos/cosmos-sdk.git
+# git clone --depth 1 https://github.com/celestiaorg/celestia-node
+# git clone --deptht 1 https://github.com/celestiaorg/celestia-core
+# git clone --depth 1 https://github.com/celestiaorg/celestia-app
+# git clone --depth 1 https://github.com/osmosis-labs/osmosis.git
