b201c13d79b2a018bd9f16b3e0efdc0e974e6a6e
wonderfan <fanjiahe2000@163.com>
2025-05-18 21:01:50 +0000

    chore: make updates

--- defi.sh
+++ defi.sh
@@ -1,3 +1,7 @@
 git clone --depth 1 https://github.com/IPOR-Labs/ipor-protocol.git
 git clone --depth 1 https://github.com/gmx-io/gmx-interface.git
 git clone --depth 1 https://github.com/gmx-io/gmx-contracts.git
+git clone --depth 1 https://github.com/gmx-io/gmx-synthetics.git
+git clone --depth 1 https://github.com/dydxprotocol/v4-web.git
+git clone --depth 1 https://github.com/dydxprotocol/v4-chain.git
+git clone --depth 1 https://github.com/dydxprotocol/v4-documentation.git
