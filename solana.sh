b97a633f0c62e55bff6fd25a8fecc651c9f4e778
wonderfan <fanjiahe2000@163.com>
2025-05-25 01:41:35 +0000

    chore: make updates

--- solana.sh
+++ solana.sh
@@ -1,3 +1,4 @@
+git clone --depth 1 https://github.com/solana-program/system.git
 git clone --depth 1 https://github.com/solana-program/token.git
 git clone --depth 1 https://github.com/solana-foundation/solana-com.git
 git clone --depth 1 https://github.com/solana-foundation/specs.git
