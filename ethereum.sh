8471400b1a790656f74dee607298b8306571b195
wonderfan <fanjiahe2000@163.com>
2025-06-08 07:45:46 +0000

    chore: make updates

--- ethereum.sh
+++ ethereum.sh
@@ -1,2 +1,5 @@
+git clone --depth 1 https://github.com/blockscout/blockscout-rs.git
+git clone --depth 1 https://github.com/blockscout/blockscout.git
+git clone --depth 1 https://github.com/EthVM/EthVM.git
 git clone --depth 1 https://github.com/prysmaticlabs/prysm.git
 git clone --depth 1 https://github.com/ethereum/go-ethereum.git
