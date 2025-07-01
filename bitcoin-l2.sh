f91783b0a566d064c8155a23e130c687dda1fb04
wonderfan <fanjiahe2000@163.com>
2025-07-01 23:04:09 +0000

    chore: make updates

--- bitcoin-l2.sh
+++
@@ -1,6 +0,0 @@
-git clone --depth 1 https://github.com/alexgo-io/alexlab-doc.git
-git clone --depth 1 https://github.com/stacks-network/docs.git
-git clone --depth 1 https://github.com/stacks-network/stacks-core.git
-git clone --depth 1 https://github.com/cloudhead/nakamoto.git
-git clone --depth 1 https://github.com/RGB-WG/rgb-core.git
-git clone --depth 1 https://github.com/lightningdevkit/rust-lightning.git
